4c4606db2901fcbe05d35a5c77832433cefd1957
Sebastienpanda <sebastienweb27@gmail.com>
2024-11-15 10:19:47 +0100

    Add storybooks project

---
+++ .storybook/preview.ts
@@ -0,0 +1,10 @@
+import type { Preview } from '@storybook/vue3';
+import '../src/ui/styles/index.css';
+
+const preview: Preview = {
+    parameters: {
+        layout: 'centered',
+    },
+};
+
+export default preview;
