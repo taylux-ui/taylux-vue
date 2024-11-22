ec5baf2d8edea89bb7601bbbd543a8a1b363269e
Sebastienpanda <sebastienweb27@gmail.com>
2024-11-22 16:12:48 +0100

    chore: modify prettier eslint and build.yml

--- eslint.config.js
+++ eslint.config.js
@@ -14,7 +14,7 @@ export default [
             '*.config.js',
             'public/**',
             'build/**',
-            '.github/worflows/publish.yml',
+            '.github/worflows/build.yml',
         ],
     },
     {
